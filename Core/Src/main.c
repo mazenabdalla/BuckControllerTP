13a4c9264f7dac5c7ad7de64484d5c21ea83f4ad
Mazen Abdalla <mazen.abdalla@rutgers.edu>
2024-03-05 22:05:46 -0500

    fixed bug with control loop speed

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -252,9 +252,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 100-1;
+  htim1.Init.Prescaler = 16-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 4-1;
+  htim1.Init.Period = 1000-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
